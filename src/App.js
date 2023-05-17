3b683e6300e911621c61f749d36cc0f046c3f3ef
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2023-05-18 01:22:02 +0530

    feature: added home about and contact page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import Contact from "./pages/Contact/Contact";
 import HomePage from "./pages/HomePage/HomePage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,18 @@ const Navbar = () => {
         <img className={styles["logo"]} src="/assets/logo.png" alt="logo" />
         <p className={styles["brand_name"]}>TripWise AI</p>
       </div>
+
+      <div className={styles["right_container"]}>
+        <a className={styles["nav_link"]} href="/">
+          Home
+        </a>
+        <a className={styles["nav_link"]} href="/about">
+          About
+        </a>
+        <a className={styles["nav_link"]} href="/contact">
+          Contact
+        </a>
+      </div>
     </nav>
   );
 };
